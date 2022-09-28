abeece83125b055e6721e45b17eafbeb4cfa84cd
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2022-09-28 17:47:54 +0200

    Setup docker (#1)
    * Setup docker
    
    * Add a bind mount for the backend app in docker
    
    * setup nexus artificats

---
+++ packages/api/nexus.gen.ts
@@ -0,0 +1,159 @@
+// @ts-nocheck
+
+import type { GraphQLContextType } from "./src/types/GraphQLContextType";
+import type { FieldAuthorizeResolver } from "nexus/dist/plugins/fieldAuthorizePlugin";
+import type { core, connectionPluginCore } from "nexus";
+
+declare global {
+  interface NexusGenCustomOutputMethods<TypeName extends string> {
+    /**
+     * Adds a Relay-style connection to the type, with numerous options for configuration
+     *
+     * @see https://nexusjs.org/docs/plugins/connection
+     */
+    connectionField<FieldName extends string>(
+      fieldName: FieldName,
+      config: connectionPluginCore.ConnectionFieldConfig<TypeName, FieldName>
+    ): void;
+  }
+}
+
+declare global {
+  interface NexusGen extends NexusGenTypes {}
+}
+
+export interface NexusGenInputs {}
+
+export interface NexusGenEnums {}
+
+export interface NexusGenScalars {
+  String: string;
+  Int: number;
+  Float: number;
+  Boolean: boolean;
+  ID: string;
+}
+
+export interface NexusGenObjects {
+  Query: {};
+}
+
+export interface NexusGenInterfaces {}
+
+export interface NexusGenUnions {}
+
+export type NexusGenRootTypes = NexusGenObjects;
+
+export type NexusGenAllTypes = NexusGenRootTypes & NexusGenScalars;
+
+export interface NexusGenFieldTypes {
+  Query: {
+    // field return type
+    ok: boolean; // Boolean!
+  };
+}
+
+export interface NexusGenFieldTypeNames {
+  Query: {
+    // field return type name
+    ok: "Boolean";
+  };
+}
+
+export interface NexusGenArgTypes {}
+
+export interface NexusGenAbstractTypeMembers {}
+
+export interface NexusGenTypeInterfaces {}
+
+export type NexusGenObjectNames = keyof NexusGenObjects;
+
+export type NexusGenInputNames = never;
+
+export type NexusGenEnumNames = never;
+
+export type NexusGenInterfaceNames = never;
+
+export type NexusGenScalarNames = keyof NexusGenScalars;
+
+export type NexusGenUnionNames = never;
+
+export type NexusGenObjectsUsingAbstractStrategyIsTypeOf = never;
+
+export type NexusGenAbstractsUsingStrategyResolveType = never;
+
+export type NexusGenFeaturesConfig = {
+  abstractTypeStrategies: {
+    isTypeOf: false;
+    resolveType: true;
+    __typename: false;
+  };
+};
+
+export interface NexusGenTypes {
+  context: GraphQLContextType;
+  inputTypes: NexusGenInputs;
+  rootTypes: NexusGenRootTypes;
+  inputTypeShapes: NexusGenInputs & NexusGenEnums & NexusGenScalars;
+  argTypes: NexusGenArgTypes;
+  fieldTypes: NexusGenFieldTypes;
+  fieldTypeNames: NexusGenFieldTypeNames;
+  allTypes: NexusGenAllTypes;
+  typeInterfaces: NexusGenTypeInterfaces;
+  objectNames: NexusGenObjectNames;
+  inputNames: NexusGenInputNames;
+  enumNames: NexusGenEnumNames;
+  interfaceNames: NexusGenInterfaceNames;
+  scalarNames: NexusGenScalarNames;
+  unionNames: NexusGenUnionNames;
+  allInputTypes:
+    | NexusGenTypes["inputNames"]
+    | NexusGenTypes["enumNames"]
+    | NexusGenTypes["scalarNames"];
+  allOutputTypes:
+    | NexusGenTypes["objectNames"]
+    | NexusGenTypes["enumNames"]
+    | NexusGenTypes["unionNames"]
+    | NexusGenTypes["interfaceNames"]
+    | NexusGenTypes["scalarNames"];
+  allNamedTypes:
+    | NexusGenTypes["allInputTypes"]
+    | NexusGenTypes["allOutputTypes"];
+  abstractTypes: NexusGenTypes["interfaceNames"] | NexusGenTypes["unionNames"];
+  abstractTypeMembers: NexusGenAbstractTypeMembers;
+  objectsUsingAbstractStrategyIsTypeOf: NexusGenObjectsUsingAbstractStrategyIsTypeOf;
+  abstractsUsingStrategyResolveType: NexusGenAbstractsUsingStrategyResolveType;
+  features: NexusGenFeaturesConfig;
+}
+
+declare global {
+  interface NexusGenPluginTypeConfig<TypeName extends string> {}
+  interface NexusGenPluginInputTypeConfig<TypeName extends string> {}
+  interface NexusGenPluginFieldConfig<
+    TypeName extends string,
+    FieldName extends string
+  > {
+    /**
+     * Authorization for an individual field. Returning "true"
+     * or "Promise<true>" means the field can be accessed.
+     * Returning "false" or "Promise<false>" will respond
+     * with a "Not Authorized" error for the field.
+     * Returning or throwing an error will also prevent the
+     * resolver from executing.
+     */
+    authorize?: FieldAuthorizeResolver<TypeName, FieldName>;
+
+    /**
+     * The nullability guard can be helpful, but is also a potentially expensive operation for lists.
+     * We need to iterate the entire list to check for null items to guard against. Set this to true
+     * to skip the null guard on a specific field if you know there's no potential for unsafe types.
+     */
+    skipNullGuard?: boolean;
+  }
+  interface NexusGenPluginInputFieldConfig<
+    TypeName extends string,
+    FieldName extends string
+  > {}
+  interface NexusGenPluginSchemaConfig {}
+  interface NexusGenPluginArgConfig {}
+}
--- packages/api/src/graphql/index.ts
+++ packages/api/src/graphql/index.ts
@@ -1,22 +1,54 @@
+import { join } from "node:path";
 import { graphqlHTTP } from "express-graphql";
-import { buildSchema } from "graphql";
+import {
+  makeSchema,
+  fieldAuthorizePlugin,
+  connectionPlugin,
+  nullabilityGuardPlugin,
+} from "nexus";
 
-const schema = buildSchema(`
-    type Query {
-        hello: String
-    }
-`);
-
-const root = {
-  hello: () => {
-    return "Hello world!";
+const schema = makeSchema({
+  types: [],
+  outputs: {
+    schema: join(__dirname, "../../nexus.gen.graphql"),
+    typegen: join(__dirname, "../../nexus.gen.ts"),
   },
-};
+  // TODO: The field authorize plugin should be configured to throw a NotPermittedError
+  // Also in future, it might be a good idea to look into query complexities
+  plugins: [
+    fieldAuthorizePlugin(),
+    connectionPlugin(),
+    nullabilityGuardPlugin({
+      onGuarded({ info }) {
+        // TODO: Report this is an error management system
+        console.error(`
+          Error: Saw a null value for non-null field ${info.parentType.name}.${info.fieldName}
+        `);
+      },
+      fallbackValues: {
+        Int: () => 0,
+        String: () => "",
+        ID: ({ info }) => `${info.parentType.name}:N/A`,
+        Boolean: () => false,
+        Float: () => 0,
+      },
+    }),
+  ],
+  sourceTypes: {
+    headers: ["// @ts-nocheck"],
+    modules: [],
+  },
+  contextType: {
+    module: join(__dirname, "../types", "GraphQLContextType.ts"),
+    export: "GraphQLContextType",
+  },
+  prettierConfig: require.resolve("../../.prettierrc"),
+  nonNullDefaults: { output: true, input: true },
+});
 
 export function applyMiddleware() {
   return graphqlHTTP({
     schema,
-    rootValue: root,
     graphiql: true,
   });
 }
