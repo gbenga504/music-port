6cd4a2564d6e8fb7d26040ad2a1aea5b4fbe1e92
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2022-11-05 21:49:43 +0100

    Rename Home to ImportHomePage

--- packages/frontend-app/src/app/routes.ts
+++ packages/frontend-app/src/app/routes.ts
@@ -3,13 +3,15 @@ import loadable from "@loadable/component";
 import type { RouteObjectWithLoadData } from "react-router-dom";
 import type { ILoadableComponentProps } from "./utils/routeUtils";
 
-import { loadData as HomeLoadData } from "./pages/Home/loadData";
+import { loadData as HomeLoadData } from "./pages/ImportHomePage/loadData";
 
 const routes: RouteObjectWithLoadData[] = [
   {
     id: "home",
     path: "/",
-    component: loadable<ILoadableComponentProps>(() => import("./pages/Home")),
+    component: loadable<ILoadableComponentProps>(
+      () => import("./pages/ImportHomePage"),
+    ),
     loadData: HomeLoadData,
   },
 ];
