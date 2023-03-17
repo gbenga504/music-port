7d96632ee5d1c65ed799ce8a7e2172f3103f4a90
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2023-03-17 14:39:54 +0100

    Create basic Drawer component

---
+++ packages/frontend-app/src/app/components/Drawer/index.tsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useRef, useState } from "react";
+import { createPortal } from "react-dom";
+import classNames from "classnames";
+
+import type { ReactNode, MouseEventHandler } from "react";
+
+import "./index.scss";
+import { isDOMLoaded } from "../../../utils/dom";
+import { sleep } from "../../../utils/sleep";
+
+interface IProps {
+  open: boolean;
+  children: ReactNode;
+  placement: "right" | "bottom";
+  onClose?: MouseEventHandler<HTMLElement>;
+}
+
+export const Drawer: React.FC<IProps> = ({
+  children,
+  placement,
+  open,
+  onClose,
+}) => {
+  const [hasDrawerBeenAppendedToBody, setHasDrawerBeenAppendedToBody] =
+    useState(false);
+  const [internallyOpen, setInternallyOpen] = useState(false);
+  const [isDrawerOnScreen, setIsDrawerOnScreen] = useState(false);
+  const portalRef = useRef(
+    isDOMLoaded() ? document.createElement("div") : null
+  );
+
+  useEffect(() => {
+    document.body.appendChild(portalRef.current!);
+    setHasDrawerBeenAppendedToBody(true);
+
+    return () => {
+      document.body.removeChild(portalRef.current!);
+    };
+  }, []);
+
+  useEffect(() => {
+    (async function () {
+      if (open) {
+        setInternallyOpen(true);
+        await sleep(200);
+
+        setIsDrawerOnScreen(true);
+        return;
+      }
+
+      setIsDrawerOnScreen(false);
+      await sleep(500);
+      setInternallyOpen(false);
+    })();
+  }, [open]);
+
+  const renderModal = () => {
+    return (
+      <div
+        className={classNames(
+          "drawer fixed pointer-events-none z-1000 w-screen h-screen top-0 left-0",
+          { open: internallyOpen }
+        )}
+        tabIndex={-1}
+      >
+        <div
+          className={classNames("drawer-overlay", {
+            "drawer-overlay-motion": isDrawerOnScreen,
+          })}
+          onClick={onClose}
+        />
+        <div
+          className={classNames(`drawer-contentContainer ${placement}`, {
+            open: internallyOpen,
+            motion: isDrawerOnScreen,
+          })}
+        >
+          <div className="w-full h-full overflow-auto bg-white pointer-events-auto">
+            <div className="flex flex-col w-full h-full">{children}</div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  if (hasDrawerBeenAppendedToBody) {
+    return createPortal(renderModal(), portalRef.current!);
+  }
+
+  return null;
+};
