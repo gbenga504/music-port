79a21a59f59c147078d6617e8b1569feb29c5406
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2023-03-17 20:23:52 +0100

    Mobile menu design

---
+++ packages/frontend-app/src/app/components/AppHeader/MobileMenu.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+import type { MouseEventHandler } from "react";
+
+import { Drawer } from "../Drawer";
+import { AnimatedHamburgerIcon } from "../icons";
+import { Button } from "../Button";
+
+interface IProps {
+  open: boolean;
+  onClose: MouseEventHandler<HTMLElement>;
+}
+
+export const MobileMenu: React.FC<IProps> = ({ open, onClose }) => {
+  const renderHeader = () => {
+    return (
+      <div className="pb-6 border-b-2 border-solid border-secondaryAlpha">
+        <AnimatedHamburgerIcon open={open} onClick={onClose} />
+      </div>
+    );
+  };
+
+  const renderBody = () => {
+    return (
+      <ul>
+        <li>
+          <Button variant="transparent" size="large" fullWidth to="#">
+            Community playlist
+          </Button>
+        </li>
+        <li>
+          <Button variant="transparent" size="large" fullWidth to="#">
+            Pricing
+          </Button>
+        </li>
+      </ul>
+    );
+  };
+
+  return (
+    <Drawer
+      open={open}
+      onClose={onClose}
+      contentContainerInnerBgColorClassName="bg-secondary"
+    >
+      <div className="p-4 mt-5">
+        {renderHeader()}
+        {renderBody()}
+      </div>
+    </Drawer>
+  );
+};
