8b7100e6119a78e9d0bf4e7045853f1103f41e72
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2023-03-21 00:59:33 +0100

    Input redesign

--- packages/frontend-app/src/app/components/Input/index.tsx
+++ packages/frontend-app/src/app/components/Input/index.tsx
@@ -1,3 +1,100 @@
-export { Input } from "./Input";
-export { InputGroup } from "./InputGroup";
-export { InputRightElement } from "./InputRightElement";
+import React, { forwardRef, useState } from "react";
+import classNames from "classnames";
+
+import type { ChangeEventHandler, ReactNode } from "react";
+import { RedStarIcon } from "../icons";
+import "./index.scss";
+
+interface IProps {
+  size?: "medium" | "small";
+  variant?: "dashed" | "outlined";
+  onChange?: ChangeEventHandler<HTMLInputElement>;
+  type?: string;
+  value?: string;
+  maxLength?: number;
+  disabled?: boolean;
+  defaultValue?: string;
+  placeholder?: string;
+  fullWidth?: boolean;
+  error?: boolean;
+  helperText?: string;
+  prefix?: ReactNode;
+  label?: string;
+  required?: boolean;
+  inputTextColor?: "black" | "white";
+  className?: string;
+}
+
+export const Input = forwardRef<HTMLInputElement, IProps>(
+  (
+    {
+      size = "medium",
+      fullWidth,
+      disabled,
+      helperText,
+      error,
+      prefix,
+      variant = "outlined",
+      label,
+      required,
+      inputTextColor = "black",
+      className,
+      ...rest
+    },
+    ref
+  ) => {
+    const [isFocused, setIsFocused] = useState(false);
+
+    return (
+      <div className="flex-1">
+        {label && (
+          <div className="flex items-start mb-2">
+            <span className="text-slate-500">{label}</span>
+            {required && (
+              <span className="ml-1">
+                <RedStarIcon size={10} />
+              </span>
+            )}
+          </div>
+        )}
+        <div
+          className={classNames("input", {
+            fullWidth,
+            small: size === "small",
+            medium: size === "medium",
+            outlined: variant === "outlined",
+            dashed: variant === "dashed",
+            disabled,
+            error,
+            focused: isFocused,
+            [`${className}`]: className,
+          })}
+        >
+          {prefix && <div className="mr-3">{prefix}</div>}
+          <input
+            ref={ref}
+            required
+            className={classNames({
+              textBlack: inputTextColor === "black",
+              textWhite: inputTextColor === "white",
+            })}
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
+            {...rest}
+            disabled={disabled}
+            placeholder="Paste playlist link"
+          />
+        </div>
+        {helperText && (
+          <p
+            className={classNames("mt-2 text-xs", {
+              "text-red-600": error === true,
+            })}
+          >
+            {helperText}
+          </p>
+        )}
+      </div>
+    );
+  }
+);
