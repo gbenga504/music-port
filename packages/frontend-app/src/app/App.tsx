2ac52b9478189445e7837b81cdb272e55a527693
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2023-04-21 23:02:21 +0200

    Fix the loader

--- packages/frontend-app/src/app/App.tsx
+++ packages/frontend-app/src/app/App.tsx
@@ -122,7 +122,7 @@ const App: React.FC<IProps> = ({ pageDatas, error, api }) => {
   return (
     <ErrorBoundary error={error}>
       {isPageLoading && (
-        <div className="absolute w-screen">
+        <div className="fixed w-screen">
           <ProgressBar variant="indeterminate" />
         </div>
       )}
