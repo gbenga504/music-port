2f68402760f42f4fade756366db706f0ccc728bf
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2023-04-22 12:40:07 +0200

    Fix minor issue with select dropdown

---
+++ packages/frontend-app/src/app/hooks/useOnClickOutside.ts
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+
+import { RefObject } from "react";
+
+type Handler = (event: MouseEvent) => void;
+
+function useOnClickOutside<T extends HTMLElement = HTMLElement>(
+  ref: RefObject<T>,
+  handler: Handler,
+  mouseEvent: "mousedown" | "mouseup" = "mousedown",
+): void {
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      const el = ref?.current;
+
+      // Do nothing if clicking ref's element or descendent elements
+      if (!el || el.contains(event.target as Node)) {
+        return;
+      }
+
+      handler(event);
+    };
+
+    window.addEventListener(mouseEvent, listener);
+
+    return () => {
+      window.removeEventListener(mouseEvent, listener);
+    };
+  }, [mouseEvent]);
+}
+
+export default useOnClickOutside;
