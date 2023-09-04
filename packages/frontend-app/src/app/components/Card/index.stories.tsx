ba88d2356717722258e0d34a572b4ee5676520d4
Afolabi Moyinoluwa Emmanuel <afolabimoyinoluwa4@gmail.com>
2023-09-04 11:51:21 +0100

    correction fix

--- packages/frontend-app/src/app/components/Card/index.stories.tsx
+++ packages/frontend-app/src/app/components/Card/index.stories.tsx
@@ -1,13 +1,30 @@
+import React from "react";
 import { Meta, StoryObj } from "@storybook/react";
 
 import { Card } from "./index";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const meta: Meta<typeof Card> = {
   title: "Card",
   component: Card,
+  decorators: [
+    (Story) => (
+      <div className="bg-black">
+        <Router>
+          <Story />
+        </Router>
+      </div>
+    ),
+  ],
 };
 export default meta;
 
 type Story = StoryObj<typeof Card>;
 
-export const Small: Story = {};
+export const BaseCard: Story = {
+  args: {
+    src: "",
+    title: "My God Is Still The Same",
+    artist: "Sanctus Real",
+  },
+};
--- packages/frontend-app/src/app/components/Card/index.tsx
+++ packages/frontend-app/src/app/components/Card/index.tsx
@@ -1,69 +1,51 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./index.scss";
+import { Button } from "../Button";
+import { Link } from "react-router-dom";
+import { AppleMusicIcon } from "../icons";
 
 interface IProps {
   src: string;
   title: string;
   artist: string;
+  link: string;
 }
 
-export const Card: React.FC<IProps> = ({
-  src,
-  title = "I told them...",
-  artist = "Burna Boy",
-}) => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  const handleMouseOver = () => {
-    setIsHovered(true);
-  };
-
-  const handleMouseOut = () => {
-    setIsHovered(false);
-  };
-
+export const Card: React.FC<IProps> = ({ src, title, artist, link }) => {
   return (
-    <div
-      className="flex flex-col cursor-pointer w-[214px]"
-      onMouseOver={handleMouseOver}
-      onMouseOut={handleMouseOut}
-    >
-      <div className="relative">
-        <img
-          src={src || "public/images/burna.jpeg"}
-          alt={title}
-          width={214}
-          height={214}
-        />
-        {isHovered && (
+    <Link to={link}>
+      <div className="card__container">
+        <div className="relative">
+          <img src={src} alt={title} className="cover__image" />
+
           <div className="overlay">
             <div className="overlay-content">
-              <button className="text-white text-sm">play</button>
-              <button className="text-white text-sm">options</button>
+              <Button
+                size="small"
+                className="text-white bg-play hover:bg-primary rounded-50 p-2.5"
+              >
+                <AppleMusicIcon />
+              </Button>
+              <Button
+                size="small"
+                className="text-white bg-play hover:bg-primary rounded-50 p-2.5"
+              >
+                Op
+              </Button>
             </div>
           </div>
-        )}
-      </div>
-      <div className="mt-1">
-        <div className="flex items-center justify-between">
-          <p className="text-[#ffffffeb] text-xs">{title}</p>
-          <div>
-            <span role="img">
-              <svg
-                className="fill-white"
-                viewBox="0 0 9 9"
-                width={9}
-                height={9}
-                aria-hidden="true"
-              >
-                <path d="M3.9 7h1.9c.4 0 .7-.2.7-.5s-.3-.4-.7-.4H4.1V4.9h1.5c.4 0 .7-.1.7-.4 0-.3-.3-.5-.7-.5H4.1V2.9h1.7c.4 0 .7-.2.7-.5 0-.2-.3-.4-.7-.4H3.9c-.6 0-.9.3-.9.7v3.7c0 .3.3.6.9.6zM1.6 0h5.8C8.5 0 9 .5 9 1.6v5.9C9 8.5 8.5 9 7.4 9H1.6C.5 9 0 8.5 0 7.4V1.6C0 .5.5 0 1.6 0z"></path>
-              </svg>
-            </span>
-          </div>
         </div>
-        <p className="mt-px text-xs text-[#ffffffa3]">{artist}</p>
+        <div className="mt-1">
+          <div className="flex items-center justify-between">
+            <p className="text-title text-xs">{title}</p>
+            <div>
+              <span className="icon"></span>
+            </div>
+          </div>
+          <p className="mt-px text-xs text-artist">{artist}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
