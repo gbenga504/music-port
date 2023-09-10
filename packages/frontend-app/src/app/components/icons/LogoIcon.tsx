9a7985dc4aa4b97266df9bf0b834e53b085d44c5
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2023-09-10 09:59:58 +0200

    Add the header to the sidebar

--- packages/frontend-app/src/app/components/icons/LogoIcon.tsx
+++ packages/frontend-app/src/app/components/icons/LogoIcon.tsx
@@ -5,8 +5,8 @@ interface IProps {}
 export const LogoIcon: React.FC<IProps> = () => {
   return (
     <div className="flex items-center gap-1">
-      <img src="/public/images/white-logo.png" className="w-8" />
-      <span className="font-light text-xl text-secondary50">MusicPort</span>
+      <img src="/public/images/white-logo.png" className="w-7" />
+      <span className="font-normal text-xl text-secondary50">Port</span>
     </div>
   );
 };
--- packages/frontend-app/src/app/components/v2/PageLayout.tsx
+++ packages/frontend-app/src/app/components/v2/PageLayout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { HeadMarkup } from "../HeadMarkup";
 import { DesktopSidebar } from "./DesktopSidebar";
+import { AppHeader } from "./AppHeader";
 
 interface IProps {
   title: string;
@@ -10,10 +11,13 @@ interface IProps {
 
 export const PageLayout: React.FC<IProps> = ({ title, description }) => {
   return (
-    <div className="mx-0 h-screen grid grid-cols-[260px_4fr]">
+    <div className={"mx-0 h-screen grid grid-cols-1 lg:grid-cols-[260px_4fr]"}>
       <HeadMarkup title={title} description={description} />
-      <div>
+      <div className="hidden lg:block">
         <DesktopSidebar />
+      </div>
+      <div>
+        <AppHeader />
       </div>
     </div>
   );
