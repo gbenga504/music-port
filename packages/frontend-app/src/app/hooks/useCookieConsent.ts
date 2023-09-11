3782b6011dcf9b6bcde3e6097bd78034751f0490
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2023-09-11 09:22:24 +0200

    Fix cookie banner (#56)

--- packages/frontend-app/src/app/hooks/useCookieConsent.ts
+++ packages/frontend-app/src/app/hooks/useCookieConsent.ts
@@ -4,35 +4,43 @@ import Cookies from "js-cookie";
 import { useApi } from "../context/ApiContext";
 
 type CookieValue = { necessary: boolean } | null;
-type CookieConsentFn = () => Promise<CookieValue>;
+type CookieConsentFn = () => Promise<void>;
+type CookieStatus =
+  | "cannot_be_determined"
+  | "user_has_not_seen_banner"
+  | "user_has_seen_banner";
 
 // TODO: We wanna add an event listener so other parts of the code can subscribe
 // to cookie change events
-function useCookieConsent(): [CookieValue, CookieConsentFn] {
+// cannot_be_determined: Means we cannot determine the current status of the cookie. Still need to know if user has seen banner or not
+// user_has_not_seen_banner: Means the user hasn't seen the banner yet so we need to know them
+// user_has_seen_banner: Means the user has seen the banner and consented
+function useCookieConsent(): [CookieValue, CookieStatus, CookieConsentFn] {
   const api = useApi();
 
-  const [isStatusKnown, setIsStatusKnown] = useState(false);
+  const [cookieStatus, setCookieStatus] = useState<CookieStatus>(
+    "cannot_be_determined",
+  );
   const [cookieValue, setCookieValue] = useState<CookieValue>(null);
 
   useEffect(() => {
-    const cookie = Cookies.get("cookieConsent") ?? null;
+    const cookie = Cookies.get("cookieConsent");
+    const parsedCookie: CookieValue = cookie ? JSON.parse(cookie) : null;
 
-    setIsStatusKnown(true);
-    setCookieValue((cookie as CookieValue) ?? null);
+    setCookieStatus(
+      parsedCookie ? "user_has_seen_banner" : "user_has_not_seen_banner",
+    );
+    setCookieValue(parsedCookie);
   }, []);
 
-  const handleConsentToCookie = async (): Promise<CookieValue> => {
+  const handleConsentToCookie = async () => {
+    setCookieStatus("user_has_seen_banner");
     const value = (await api.cookieConsent.cookieConsent()) as CookieValue;
 
-    setIsStatusKnown(true);
     setCookieValue(value);
-    return value;
   };
 
-  return [
-    isStatusKnown ? cookieValue : { necessary: false },
-    handleConsentToCookie,
-  ];
+  return [cookieValue, cookieStatus, handleConsentToCookie];
 }
 
 export default useCookieConsent;
