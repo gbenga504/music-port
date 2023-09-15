7d0ca908ef7e9eaa7af8d159c3ae1e8868068c12
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2023-09-15 15:01:58 +0200

    Fix banner (#59)

--- packages/frontend-app/src/app/components/CookieBanner.tsx
+++ packages/frontend-app/src/app/components/CookieBanner.tsx
@@ -14,17 +14,17 @@ export const CookieBanner = () => {
   return cookieStatus === "user_has_not_seen_banner" ? (
     <div
       className={classNames(
-        "bg-white p-3 flex flex-col md:flex-row justify-between items-center",
+        "bg-primary p-3 flex flex-col md:flex-row justify-between items-center",
         "fixed bottom-0 left-0 w-full"
       )}
     >
-      <p className="text-black w-full md:w-2/4">
+      <p className="text-white w-full md:w-2/4 font-light">
         We use necessary cookies to make our site work
       </p>
       <div className="mt-3 md:mt-0">
         <Button
           variant="contained"
-          color="primary"
+          color="secondary"
           size="small"
           onClick={handleConsent}
         >
