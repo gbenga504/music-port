d5ea87860c9db921a7e6367261ca314c05ec16ff
Emmanuel <36973745+emmacode@users.noreply.github.com>
2023-09-18 15:41:23 +0100

    CardLists component (#64)
    * CardLists component
    
    * adjustment to eslint rules
    
    * removed unnecessary code in css and Cardlist.tsx
    
    * Link tag wrapped around section header

---
+++ packages/frontend-app/src/app/components/CardLists/CardLists.stories.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import { CardLists } from "./CardLists";
+
+import { Card } from "../Card/Card";
+
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof CardLists> = {
+  title: "Card Lists",
+  component: CardLists,
+  decorators: [
+    (Story) => (
+      <div className="bg-black w-screen h-screen fixed top-0 left-0 flex items-center justify-center">
+        <Router>
+          <Story />
+        </Router>
+      </div>
+    ),
+  ],
+};
+export default meta;
+
+type Story = StoryObj<typeof CardLists>;
+
+export const BaseContainer: Story = {
+  render: () => (
+    <CardLists title="Gospel" to="#">
+      <li className="w-[350px]">
+        <Card
+          src="https://images.prismic.io/wunderflatscontent/ef754f0d-5903-40b7-a1d6-ea143f480f37_Balcony-M.jpg?auto=compress,format"
+          title="My God Is Still The Same"
+          artist="Sanctus Real"
+          link="#"
+        />
+      </li>
+      <li className="w-[350px]">
+        <Card
+          src="https://images.prismic.io/wunderflatscontent/ef754f0d-5903-40b7-a1d6-ea143f480f37_Balcony-M.jpg?auto=compress,format"
+          title="My God Is Still The Same"
+          artist="Sanctus Real"
+          link="#"
+        />
+      </li>
+      <li className="w-[350px]">
+        <Card
+          src="https://images.prismic.io/wunderflatscontent/ef754f0d-5903-40b7-a1d6-ea143f480f37_Balcony-M.jpg?auto=compress,format"
+          title="My God Is Still The Same"
+          artist="Sanctus Real"
+          link="#"
+        />
+      </li>
+      <li className="w-[350px]">
+        <Card
+          src="https://images.prismic.io/wunderflatscontent/ef754f0d-5903-40b7-a1d6-ea143f480f37_Balcony-M.jpg?auto=compress,format"
+          title="My God Is Still The Same"
+          artist="Sanctus Real"
+          link="#"
+        />
+      </li>
+      <li className="w-[350px]">
+        <Card
+          src="https://images.prismic.io/wunderflatscontent/ef754f0d-5903-40b7-a1d6-ea143f480f37_Balcony-M.jpg?auto=compress,format"
+          title="My God Is Still The Same"
+          artist="Sanctus Real"
+          link="#"
+        />
+      </li>
+      <li className="w-[350px]">
+        <Card
+          src="https://images.prismic.io/wunderflatscontent/ef754f0d-5903-40b7-a1d6-ea143f480f37_Balcony-M.jpg?auto=compress,format"
+          title="My God Is Still The Same"
+          artist="Sanctus Real"
+          link="#"
+        />
+      </li>
+    </CardLists>
+  ),
+};
