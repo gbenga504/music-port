7a0599f133d91d57deacf91f4e7fefc60e8b1ceb
Afolabi Moyinoluwa Emmanuel <afolabimoyinoluwa4@gmail.com>
2023-09-23 23:59:08 +0100

    button for user to click to display toast in ToastStories

--- packages/frontend-app/src/app/components/Toast/Toast.stories.tsx
+++ packages/frontend-app/src/app/components/Toast/Toast.stories.tsx
@@ -1,6 +1,8 @@
+import React, { useState } from "react";
 import { Toast } from "./Toast";
 
 import type { Meta, StoryObj } from "@storybook/react";
+import { Button } from "../Button/Button";
 
 const meta: Meta<typeof Toast> = {
   title: "Toast",
@@ -10,12 +12,62 @@ const meta: Meta<typeof Toast> = {
 export default meta;
 type Story = StoryObj<typeof Toast>;
 
+const ButtonWithHooks = () => {
+  const [open, setOpen] = useState(false);
+  const [status, setStatus] = useState<
+    "success" | "info" | "error" | "warning"
+  >();
+
+  return (
+    <>
+      <Button
+        onClick={() => {
+          setOpen(true);
+          setStatus("success");
+        }}
+        className="mr-3"
+      >
+        Success Toast
+      </Button>
+      <Button
+        onClick={() => {
+          setOpen(true);
+          setStatus("info");
+        }}
+        className="mr-3"
+      >
+        Info Toast
+      </Button>
+      <Button
+        onClick={() => {
+          setOpen(true);
+          setStatus("error");
+        }}
+        className="mr-3"
+      >
+        Error Toast
+      </Button>
+      <Button
+        onClick={() => {
+          setOpen(true);
+          setStatus("warning");
+        }}
+      >
+        Warning Toast
+      </Button>
+      {open && (
+        <Toast
+          title="Toast"
+          description="Toast me :)"
+          status={status}
+          position="top"
+          onClose={() => setOpen(false)}
+        />
+      )}
+    </>
+  );
+};
+
 export const BaseToast: Story = {
-  args: {
-    title: "Clipboard",
-    description: "Link copied to clipboard",
-    status: "success",
-    position: "top",
-    duration: 15000,
-  },
+  render: () => <ButtonWithHooks />,
 };
